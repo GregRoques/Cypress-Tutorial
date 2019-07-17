bd4219fb2166d62afe668e46ab0fa5ad0b4fb206
Greg Roques <greg.roques@gmail.com>
2019-07-17 12:39:55 -0400

    reviewing basics

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,5 +1,22 @@
 describe('my first cypress test', ()=>{
     it('should load example web page', ()=>{
-        cy.visit('http://example.com')
+       
+        cy.visit('/')  //does page load
+
+        cy.url().should('include', 'example.com/') //check url
+        cy.title().should('eq', "Example Domain") //check page title
+
+        // cy.wait(3000) // static wait... not a best practice
+        // cy.pause() //much better than wait...similar to Google DevTools
+
+        // cy.get('h1').contains('Example Domain')  //Check for visibility
+        // Using Aliases:    
+        cy.get('h1').as('title')
+        cy.get('@title').contains('Example Domain')
+        cy.get('@title').should('be.visible')
+    })
+
+    it('should load another superdomain', ()=>{
+        cy.visit('https://www.dev.to')
     })
 })
